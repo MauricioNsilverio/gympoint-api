c44015d2f050548e34f4216c6359e958bbceb9c4
mauricionsilverio <mauricionsilverio@gmail.com>
2019-11-07 16:05:24 -0300

    enrollments index and store methods

---
+++ src/app/controllers/EnrollmentController.js
@@ -0,0 +1,86 @@
+import * as Yup from 'yup';
+import { addMonths, isBefore, parseISO, startOfDay } from 'date-fns';
+
+import Enrollment from '../models/Enrollment';
+import Student from '../models/Student';
+import Plan from '../models/Plan';
+
+class EnrollmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const enrollments = await Enrollment.findAll({
+      order: ['id'],
+      attributes: ['id', 'start_date', 'end_date', 'price'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: Student,
+          as: 'student',
+          attributes: ['id', 'name', 'email', 'age', 'weight', 'height'],
+        },
+        {
+          model: Plan,
+          as: 'plan',
+          attributes: ['id', 'title', 'duration', 'price'],
+        },
+      ],
+    });
+
+    return res.json(enrollments);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required(),
+      plan_id: Yup.number().required(),
+      start_date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
+    const { student_id, plan_id, start_date } = req.body;
+
+    const student = await Student.findByPk(student_id);
+    if (!student) {
+      return res.status(400).json({ error: 'Student does not exist' });
+    }
+
+    const plan = await Plan.findByPk(plan_id);
+    if (!plan) {
+      return res.status(400).json({ error: 'Plan does not exist' });
+    }
+
+    const hasEnrollment = await Enrollment.findOne({ where: { student_id } });
+    if (hasEnrollment) {
+      return res
+        .status(400)
+        .json({ error: 'Student has already been enrolled' });
+    }
+
+    const parsedStartDate = startOfDay(parseISO(start_date));
+
+    if (isBefore(parsedStartDate, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
+    const end_date = addMonths(parsedStartDate, plan.duration);
+
+    const price = plan.price * plan.duration;
+
+    const enrollment = await Enrollment.create({
+      student_id,
+      plan_id,
+      start_date: parsedStartDate,
+      end_date,
+      price,
+    });
+
+    return res.json(enrollment);
+  }
+}
+
+export default new EnrollmentController();
--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import EnrollmentController from './app/controllers/EnrollmentController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -23,5 +24,10 @@ routes.get('/plans', PlanController.index);
 routes.post('/plans', PlanController.store);
 routes.put('/plans/:id', PlanController.update);
 routes.delete('/plans/:id', PlanController.delete);
+
+routes.get('/enrollments', EnrollmentController.index);
+routes.post('/enrollments', EnrollmentController.store);
+// routes.put('/enrollments', EnrollmentController.update);
+// routes.delete('/enrollments', EnrollmentController.delete);
 
 export default routes;
